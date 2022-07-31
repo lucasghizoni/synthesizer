04e1d5bb8561e3dcbd42e8efe58a0e7c4f8970e2
lucasghizoni <lucasghizoni@gmail.com>
2022-07-31 23:32:22 +0200

    apply css pattern

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
-import './App.css';
+
+import styles from './App.module.css';
 import { Keys } from "./features/keys/Keys";
 import {useAudioContext} from "./app/useAudioContext";
 import { Knob } from "./features/knob/Knob";
@@ -10,31 +11,29 @@ function App() {
   const { init, play, stop } = useAudioContext();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        {
-          showKeyboard ?
-            <div className="App-synth">
-              <Panel className="App-knobsPanel">
-                <Knob label="Volume"/>
-                <Knob label="Filter"/>
-              </Panel>
-              <Panel>
-                <Keys
-                  onMouseLeave={() => {
-                    stop();
-                  }}
-                  onMouseOver={freq => {
-                    play(freq);
-                  }}
-                />
-              </Panel>
-            </div>
-             :
-            <button onClick={() => { setShowKeyboard(true); init(); }}>start</button>
-        }
-      </header>
-    </div>
+    <section className={styles.main}>
+      {
+        showKeyboard ?
+          <div className={styles.synth}>
+            <Panel className={styles.controllersPanel}>
+              <Knob label="Volume"/>
+              <Knob label="Filter"/>
+            </Panel>
+            <Panel>
+              <Keys
+                onMouseLeave={() => {
+                  stop();
+                }}
+                onMouseOver={freq => {
+                  play(freq);
+                }}
+              />
+            </Panel>
+          </div>
+           :
+          <button onClick={() => { setShowKeyboard(true); init(); }}>start</button>
+      }
+    </section>
   );
 }
 
